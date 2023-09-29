d2cf5bfdebaa4c2731c14bd3f2066bc14eebfc56
Yiğit Tanyel <yigittanyel@trex.com.tr>
2023-09-29 09:45:48 +0300

    I tried to use refit library in a console project on the api where i approach from jsonplaceholder.

---
+++ TrialRefit/Program.cs
@@ -0,0 +1,14 @@
+﻿using Refit;
+using TrialRefit.IPostApi;
+
+var api=RestService.For<IPostApi>("https://jsonplaceholder.typicode.com");
+
+var posts = await api.GetPosts();
+
+foreach(var post in posts)
+{
+    Console.WriteLine($"Post Id:{post.Id} \nTitle: {post.Title} \nBody: {post.Body}");
+    Console.WriteLine();
+}
+
+Console.ReadKey();
